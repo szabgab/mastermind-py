3b4f5d48a2c91132b23b030486b98502efcc27b7
Gabor Szabo <gabor@szabgab.com>
2022-12-14 06:51:08 +0200

    add test with fixed seed

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,3 +1,4 @@
+import random
 import mastermind.game as mm
 
 def test_game():
@@ -9,3 +10,10 @@ def test_new_secret():
     assert len(secret) == mm.SIZE
     assert len(set(secret)) == mm.SIZE
     assert set(secret).issubset(mm.POOL)
+
+def test_fixed_secret():
+    random.seed(42)
+    secret = mm.new_secret()
+    # print(secret)
+    assert secret == [6, 1, 5, 3]
+
