673e77ec305a60271d08a19db456da7dfb74857d
Gabor Szabo <gabor@szabgab.com>
2022-12-14 06:20:38 +0200

    add function generating new secret

--- mastermind/game.py
+++ mastermind/game.py
@@ -1,2 +1,10 @@
+import random
+
+
 def play():
     ...
+
+
+def new_secret():
+    pool = list(range(1, 7))
+    return random.sample(pool, 4)
--- tests/test_game.py
+++ tests/test_game.py
@@ -3,3 +3,9 @@ import mastermind.game as mm
 def test_game():
     mm.play()
     assert True
+
+def test_new_secret():
+    secret = mm.new_secret()
+    assert len(secret) == 4
+    assert len(set(secret)) == 4
+    assert set(secret).issubset(list(range(1, 7)))
