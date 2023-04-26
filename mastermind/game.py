af99ec1b4ec6e95e1dc57d6f8d49b29203ddb464
Gabor Szabo <gabor@szabgab.com>
2023-04-26 11:24:25 +0300

    validate guess

--- mastermind/game.py
+++ mastermind/game.py
@@ -9,3 +9,18 @@ def play():
 
 def new_secret():
     return random.sample(POOL, SIZE)
+
+def validate_guess(guess, secret):
+    if len(guess) < SIZE:
+        return "Not enough values"
+    if len(guess) > SIZE:
+        return "Too many values"
+    if len(set(guess)) < SIZE:
+        seen = set()
+        for val in guess:
+            if val in seen:
+                return f"Duplicate value '{val}' in guess"
+            seen.add(val)
+    for val in guess:
+        if val not in POOL:
+            return f"Invalid value '{val}' in guess"
--- tests/test_game.py
+++ tests/test_game.py
@@ -17,3 +17,14 @@ def test_fixed_secret():
     # print(secret)
     assert secret == [6, 1, 5, 3]
 
+def test_validate_guess():
+    assert mm.validate_guess(guess=[1, 2, 3], secret=[1, 2, 3, 4]) == "Not enough values"
+    assert mm.validate_guess(guess=[1, 2, 3, 4, 5], secret=[1, 2, 3, 4]) == "Too many values"
+    assert mm.validate_guess(guess=[1, 2, 3, 3], secret=[1, 2, 3, 4]) == "Duplicate value '3' in guess"
+    assert mm.validate_guess(guess=[1, 2, 3, 7], secret=[1, 2, 3, 4]) == "Invalid value '7' in guess"
+
+    #assert mm.validate_guess(guess=[1, 2, 3, 4], secret=[1, 2, 3, 4]) == ['*', '*', '*', '*']
+    #assert mm.validate_guess(guess=[1, 2, 5, 6], secret=[1, 2, 3, 4]) == ['*', '*']
+    #assert mm.validate_guess(guess=[1, 5, 3, 6], secret=[1, 2, 3, 4]) == ['*', '*']
+    #assert mm.validate_guess(guess=[1, 3, 4, 2], secret=[1, 2, 3, 4]) == ['*', '+', '+', '+']
+    #assert mm.validate_guess(guess=[5, 4, 1, 6], secret=[1, 2, 3, 4]) == ['+', '+']
